32c215ff729c46725d7eb64fad18e11ee1e871e9
Jason <jason.dixon.email@gmail.com>
2015-11-01 18:40:23 +1300

    progress bar

--- scan.py
+++ scan.py
@@ -1,5 +1,6 @@
 # Check with brute force the distance of objects
 import math
+import time
 import maya.cmds as cmds
 from pprint import pprint
 
@@ -13,3 +14,30 @@ class AutoKey(object):
         cmds.autoKeyframe(st=False)
     def __exit__(s, *err):
         cmds.autoKeyframe(st=s.state)
+
+class Progress(object):
+    def __init__(s, title):
+        s.title = title
+        s.canceled = False
+    def __enter__(s):
+        s.win = cmds.window(t=s.title)
+        cmds.columnLayout(w=200, bgc=(0.2,0.2,0.2))
+        s.bar = cmds.columnLayout(w=1, h=40, bgc=(0.8,0.4,0.5))
+        cmds.showWindow(s.win)
+        return s
+    def update(s, progress):
+        if not progress: progress = 1
+        if cmds.layout(s.bar, ex=True):
+            cmds.columnLayout(s.bar, e=True, w=progress * 2)
+        else:
+            s.canceled = True
+        cmds.refresh()
+    def __exit__(s, *err):
+        if cmds.window(s.win, ex=True): cmds.deleteUI(s.win)
+
+with Progress("prog") as p:
+    for i in range(100):
+        time.sleep(0.1)
+        print i
+        p.update(i)
+        if p.canceled: break
