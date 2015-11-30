3a79a5891616e8f7d524878cd34436556cea776a
Internetimagery <jason.dixon.email@gmail.com>
2015-12-01 03:08:32 +1300

    sped up with maya api

--- scan.py
+++ scan.py
@@ -23,15 +23,50 @@ import maya.cmds as cmds
 import maya.api.OpenMaya as om
 from pprint import pprint as print
 
-@contextlib.contextmanager
-def temp_obj(num=1):
-    """ Create some temporary objects """
-    objs = [cmds.group(em=True) for a in range(num)]
-    try:
-        yield objs
-    finally:
-        for o in objs:
-            if cmds.objExists(o): cmds.delete(o)
+def getNode(name):
+    """ Get Node """
+    sel = om.MSelectionList()
+    sel.add(name)
+    return sel.getDependNode(0)
+
+def getPlug(obj, attr):
+    """ Get attribute from mayas API ugh """
+    obj = getNode(obj)
+    func = om.MFnDependencyNode(obj)
+    attr = func.attribute(attr)
+    return om.MPlug(obj, attr)
+
+class Attribute(object):
+    """ An Attribute """
+    threshold = 0.001 # Negate tiny adjustments
+    def __init__(s, obj, attr):
+        s.obj, s.attr = obj, attr
+        s.min = -9999999; s.max = 9999999
+        query = functools.partial(cmds.attributeQuery, attr, n=obj)
+        if not query(ex=True): raise RuntimeError, "%s does not exist." % attr
+        if query(mne=True): s.min = query(min=True)
+        if query(mxe=True): s.max = query(max=True)
+        s.plug = plug = getPlug(obj, attr)
+        s.current = plug.asDouble()
+    def __str__(s): return s.plug.name()
+    def __call__(s, val=None):
+        """ Use to query or set the attribute """
+        current = s.current
+        if val is None: return current
+        threshold = s.threshold
+        val = max(s.min, min(val, s.max))
+        if val < current - threshold or current + threshold < val:
+            s.plug.setDouble(val); s.current = val
+
+class Model(object):
+    """ A model """
+    def __init__(s, name):
+        obj = getNode(name)
+        s.transform = om.MFnTransform(om.MDagPath.getAPathTo(obj))
+    def __str__(s): return s.transform.name()
+    def __call__(s):
+        """ Get position of object """
+        return s.transform.translation(om.MSpace.kWorld)
 
 @contextlib.contextmanager
 def safe_state():
@@ -46,42 +81,13 @@ def safe_state():
         if err: cmds.undo()
         cmds.autoKeyframe(st=state)
 
-class Attribute(object):
-    """ An Attribute """
-    threshold = 0.001 # Negate tiny adjustments
-    def __init__(s, obj, attr):
-        s.obj, s.attr = obj, attr
-        s.min = -9999999; s.max = 9999999
-        query = functools.partial(cmds.attributeQuery, attr, n=obj)
-        if not query(ex=True): raise RuntimeError, "%s does not exist." % attr
-        if query(mne=True): s.min = query(min=True)
-        if query(mxe=True): s.max = query(max=True)
-        s.name = name = "%s.%s" % (obj, attr)
-        s.current = cmds.getAttr(name)
-    def __str__(s): return str(s.name)
-    def __call__(s, val=None):
-        """ Use to query or set the attribute """
-        current, threshold = s.current, s.threshold
-        if val is None: return current
-        val = max(s.min, min(val, s.max))
-        if val < current - threshold or current + threshold < val:
-            cmds.setAttr(s, val); s.current = val
-
-class Model(object):
-    """ A model """
-    def __init__(s, name):
-        s.name = name
-    def __str__(s): return str(s.name)
-    def __call__(s):
-        """ Get position of object """
-        return om.MVector(cmds.xform(s.name, q=True, ws=True, rp=True))
-
 class Scanner(object):
     """ Scan through attributes """
-    def __init__(s, objs, attrs):
-        s.objs = objs = [Model(o) for o in objs]
+    def __init__(s, attrs, objs1, objs2=None): # Primary / Secondary objs
+        s.objs1 = objs1 = [Model(o) for o in objs1]
+        s.objs2 = objs2 = [Model(o) for o in objs2] if objs2 else []
         s.attrs = attrs = [Attribute(o, at) for o, at in attrs]
-        s.movement = s.calibrate(attrs, objs)
+        s.movement = s.calibrate(attrs, objs1 + objs2)
 
     def calibrate(s, attrs, objs):
         """ Work out how much each combination moves and normalize """
@@ -124,70 +130,69 @@ class Scanner(object):
 
     def walk(s):
         """ Move to location """
-        objs, attrs = s.objs, s.attrs
+        objs1, objs2, attrs = s.objs1, s.objs2, s.attrs
         movement = s.movement
-        heuristic = s.heuristic
 
         with safe_state():
-            with temp_obj(1) as tmp:
-                start_time = time.time() # Start the clock!
-                timeout = 6 # Seconds
-                count = 0 # Combination count
-                success = 1 # Success count
-                threshold = 0.001 # How close is made it?
+            start_time = time.time() # Start the clock!
+            timeout = 6 # Seconds
+            count = 0 # Combination count
+            success = 1 # Success count
 
-                # Find center
-                center = tmp[0]
-                for o in objs: cmds.pointConstraint(o, center) # Position obj
+            def heuristic(pos): # Get value of position
+                # Move into location
+                for at, p in zip(attrs, pos): at(p)
+                dist = (objs1[0]() - objs1[1]()).length()
+                return dist
 
-                # Record our start location.
-                base_position = tuple(a() for a in attrs) # Where we start from
-                base_distance = heuristic(base_position, center) # priority
-                base_stride = base_distance * 0.25 # Step length
-                start_node = (base_distance, base_position, base_stride)
+            # Record our start location.
+            base_position = tuple(a() for a in attrs) # Where we start from
+            base_distance = heuristic(base_position) # priority
+            base_stride = base_distance * 0.25 # Step length
+            start_node = (base_distance, base_position, base_stride)
 
-                visited = set() # Don't retrace our steps
-                to_visit = [start_node] # Where to next?
-                closest = start_node # Closest position so far
+            visited = set() # Don't retrace our steps
+            to_visit = [start_node] # Where to next?
+            closest = start_node # Closest position so far
 
-                print("Walking...")
-                try:
-                    while len(to_visit):
-                        curr_dist, curr_pos, curr_stride = heapq.heappop(to_visit)
-                        path_end = True
-                        for step in movement: # Check immediate surroundings
-                            new_pos = tuple(curr_stride * a + b for a, b in zip(step, curr_pos))
-                            if new_pos not in visited: # Don't backtrack
-                                visited.add(new_pos)
-                                count += 1
-                                new_dist = heuristic(new_pos, center)
-                                new_node = (new_dist, new_pos, curr_stride)
-                                heapq.heappush(to_visit, new_node) # Mark on map
-                                if new_dist < threshold: raise StopIteration # We made it!
-                                if new_dist < curr_dist: # Are we closer?
-                                    path_end = False # We have somewhere to go!
-                                if new_dist < closest[0]: # Are we the closest we have ever been?
-                                    closest = new_node
-                                    success += 1
-                                    cmds.refresh() # Update view
-                        if path_end: # Are we at a deadend?
-                            new_stride = curr_stride * 0.5 # Narrow Search
-                            if 0.001 < new_stride:
-                                new_node = (curr_dist, curr_pos, new_stride)
-                                heapq.heappush(to_visit, new_node)
+            print("Walking...")
+            try:
+                while len(to_visit):
+                    curr_dist, curr_pos, curr_stride = heapq.heappop(to_visit)
+                    path_end = True
+                    for step in movement: # Check immediate surroundings
+                        new_pos = tuple(curr_stride * a + b for a, b in zip(step, curr_pos))
+                        if new_pos not in visited: # Don't backtrack
+                            visited.add(new_pos)
+                            count += 1
+                            new_dist = heuristic(new_pos)
+                            new_node = (new_dist, new_pos, curr_stride)
+                            heapq.heappush(to_visit, new_node) # Mark on map
+                            if new_dist < 0.001: raise StopIteration # We made it!
+                            if new_dist < curr_dist: # Are we closer?
+                                path_end = False # We have somewhere to go!
+                            if new_dist < closest[0]: # Are we the closest we have ever been?
+                                closest = new_node
+                                success += 1
+                                cmds.refresh() # Update view
+                    if path_end: # Are we at a deadend?
+                        new_stride = curr_stride * 0.5 # Narrow Search
+                        if 0.001 < new_stride:
+                            new_node = (curr_dist, curr_pos, new_stride)
+                            heapq.heappush(to_visit, new_node)
 
-                        elapsed_time = time.time() - start_time
-                        if timeout < elapsed_time: # Important!
-                            print("Timed out...")
-                            break
-                except StopIteration:
-                    print("Made it!")
-                for at, pos in zip(attrs, closest[1]):
-                    at(pos); cmds.setKeyframe(at)
-                total_time = (time.time() - start_time) * 1000
-                print("Travel complete with a time of %s ms." % total_time)
-                print("Finished with a distance of %s." % closest[0])
-                print("Made %s attempts to get there with a time of %s ms per attempt." % (count, (total_time / count)))
+                    elapsed_time = time.time() - start_time
+                    if timeout < elapsed_time: # Important!
+                        print("Timed out...")
+                        break
+            except StopIteration:
+                print("Made it!")
+            for at, pos in zip(attrs, closest[1]):
+                at(pos); cmds.setKeyframe(at)
+            total_time = (time.time() - start_time) * 1000
+            print("Travel complete with a time of %s ms." % total_time)
+            print("Finished with a distance of %s." % closest[0])
+            print("Made %s attempts to get there with a time of %s ms per attempt." % (count, (total_time / count)))
 
 # TODO:
 # Add secondary object check (pole vector)
@@ -225,7 +230,7 @@ if __name__ == '__main__':
             (objA, "rotateY"),
             (objA, "rotateZ"),
         )
-        scan = Scanner(objs, attrs)
+        scan = Scanner(attrs, objs)
         scan.walk()
         time.sleep(3)
     print("Tests complete!")
