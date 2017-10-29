d81f9f15d71513c844ed175506e322d5fd662394
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-29 21:55:04 +1300

    stuff

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import maya.cmds as cmds
 import random
+import groups
 
 def position(min_, max_):
     """ Return random position """
@@ -22,33 +23,47 @@ def scene_setup():
     cmds.parent(s3, s2)
     return s1, s2, s3
 
+#
+# You could use the progressWindow() to cancel loops with ESC, might involve some more lines but does not slow down loops while accessing and checking files.
+# import maya.cmds as cmds
+#
+# cmds.progressWindow(isInterruptable=1)
+# while 1 :
+# print "kill me!"
+# if cmds.progressWindow(query=1, isCancelled=1) :
+# break
+#
+# cmds.progressWindow(endProgress=1)
+#
+# And if you dont want to see the tiny window you could instead use mayas own progress bar inside the main windows help line with progressBar()
+
 def test_match(s1, s2, s3):
     """ Ideal matching situation. Linear movement and can 100% match."""
-    return Group(
+    return groups.Group(
         (s1, s2),
         ((s1, "tx"), (s1, "ty"), (s1, "tz")))
 
 def test_chase(s1, s2, s3):
     """ Match is possible. But certain combinations can lead to a cat/mouse chase. """
-    return Group(
+    return groups.Group(
         (s1, s2),
         ((s1, "tx"), (s1, "ty"), (s1, "tz"), (s2, "tx"), (s2, "ty"), (s2, "tz")))
 
 def test_parallel(s1, s2, s3):
     """ Parallel movement. Distance will never close. """
-    return Group(
+    return groups.Group(
         (s1, s2),
         ((s1, "tx"), (s2, "tx")))
 
 def test_lookat(s1, s2, s3):
     """ Cannot reach target, but can reach a point of minimal distance. """
-    return Group(
+    return groups.Group(
         (s1, s3),
         ((s2, "rx"), (s2, "ry")))
 
 def test_possibilities(s1, s2, s3):
     """ Many possibilities exist. """
-    return Group(
+    return groups.Group(
         (s1, s3),
         ((s1, "tx"), (s1, "ty"), (s1, "tz"), (s2, "rx"), (s2, "ry"), (s2, "rz")))
 
