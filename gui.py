0ecd4fab02cad8d549a5079426a779e9bde93c0e
Jason Dixon <jason.dixon.email@gmail.com>
2017-11-01 19:39:49 +1300

    Adding progress indicator

---
+++ gui.py
@@ -0,0 +1,42 @@
+# Prepare and run gui!
+
+from __future__ import print_function
+import maya.cmds as cmds
+import maya.mel as mel
+import contextlib
+
+
+@contextlib.contextmanager
+def progress():
+    """ Safely run operations in the scene. Clean up afterwards if errors occurr """
+
+    def update(val):
+        """ Update progress. Expect value 0 ~ 1 """
+        if cmds.progressBar(gMainProgressBar, query=True, isCancelled=True):
+            raise KeyboardInterrupt
+        cmds.progressBar(gMainProgressBar, edit=True, progress=val * 100)
+
+    err = cmds.undoInfo(openChunk=True)
+    state = cmds.autoKeyframe(q=True, state=True)
+    cmds.refresh(suspend=True) # Careful with this. Some depend nodes may not update as expected.
+    cmds.autoKeyframe(state=False)
+    gMainProgressBar = maya.mel.eval('$tmp = $gMainProgressBar')
+    cmds.progressBar( gMainProgressBar,
+    				edit=True,
+    				beginProgress=True,
+    				isInterruptable=True,
+    				status='Matching ...',
+    				maxValue=100 )
+    try:
+        yield update
+    except KeyboardInterrupt:
+        pass
+    except Exception as err:
+        raise
+    finally:
+        cmds.progressBar(gMainProgressBar, edit=True, endProgress=True)
+        cmds.autoKeyframe(state=state)
+        cmds.refresh(suspend=False)
+        cmds.undoInfo(closeChunk=True)
+        if err:
+            cmds.undo()
