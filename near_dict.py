a434f3fd5fba89c24291af0cb35ecb3485a065f7
Jason Dixon <jdixon@wetafx.co.nz>
2017-11-03 17:24:28 +1300

    slim down code

--- near_dict.py
+++ near_dict.py
@@ -1,4 +1,4 @@
-# Dict that gets values based on nearest key.
+# Dict that takes vectors as keys and gets values based on nearest key.
 from __future__ import print_function, division
 import math
 
@@ -6,10 +6,8 @@ def distance(vec1, vec2):
     return math.sqrt(sum((b - a)**2 for a, b in zip(vec1, vec2)))
 
 class Dict(dict):
-    def near(s, *key):
-        """ Return nearest keys value """
-        # grab all nearby keys
-        nearest = [(distance(key, k), v) for k, v in s.items()]
-        total = sum(a[0] for a in nearest)
-        scale = 1 / total
-        return sum(k * scale * v for k, v in nearest)
+    def near(s, *vec):
+        """ Return nearest keys value. """
+        nearest = [(distance(vec, a), b) for a, b in s.items()]
+        scale = 1 / sum(a for a, b in nearest)
+        return sum(a * scale * b for a, b in nearest)
