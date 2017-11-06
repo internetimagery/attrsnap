a14a52b76b79651641f97ed345d6ce97601b3ce0
Jason Dixon <jdixon@wetafx.co.nz>
2017-11-06 20:01:53 +1300

    more heh

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@
 import datetime
 import re
 
-PRIORITY = re.compile(r"(x|\([A-Z]\))\s")
+PRIORITY = re.compile(r"^(x|\(([A-Z])\))")
 DATE = re.compile(r"(\d{4})-(\d{2})-(\d{2})\s")
 PROJECT = re.compile(r"\s\+([^\s]+)")
 CONTEXT = re.compile(r"\s@([^\s]+)")
@@ -10,25 +10,45 @@ KEYVAL = re.compile(r"\s([^\s:]+):([^\s:]+)")
 
 EXTRA = re.compile(r"(\+|@|[^\s:]:)([^\s]+)")
 
+
+https://github.com/EpocDotFr/todotxtio/blob/master/todotxtio.py
+
 class Todo(object):
     """ Todo obj """
     def __init__(s):
-        s.priority = None
-        s.complete = False
-        s.creation = None
-        s.note = ""
-        s.projects = []
-        s.contexts = []
-        s.extra = {}
+        s.reset()
+
+    def reset(s):
+        """ Set default values """
+        s._priority = None
+        s._done = False
+        s._creation = None
+        s._complete = None
+        s._task = ""
+        s._projects = []
+        s._contexts = []
+        s._extra = {}
 
     def parse(s, line):
         """ Parse line of todo """
+        tokens = re.split(r"\s", line)
+        num_tokens = len(tokens)
+        task = []
 
-        if line[0] == "x": # Note is complete
-            pass
-        else:
-            pass
+        if num_tokens > 0:
+            # Check for completed or priority
+            token = PRIORITY.match(tokens[0])
+            if token:
+                if token.group(1) == "x":
+                    s._done = True
+                else:
+                    s._priority = token.group(2)
 
+            token = DATE.match(tokens[1])
+            if token:
+                date = datetime.date(token.group(1), token.group(2), token.group(3))
+                if s._done: # Date is completion date
+                    s._complete = date
 
 # Rule 1: If priority exists, it ALWAYS appears first.
 
@@ -44,13 +64,13 @@ class Todo(object):
 #     A task may have zero, one, or more than one projects and contexts included in it.
 
 
-# (A) Thank Mom for the meatballs @phone
-# (B) Schedule Goodwill pickup +GarageSale @phone
-# Post signs around the neighborhood +GarageSale
-# @GroceryStore Eskimo pies
-# x 2011-03-02 2011-03-01 Review Tim's pull request +TodoTxtTouch @github
-# Get things done @home
-# Get some other things done @work
-# Add task text to Chromodoro @home +Chromodoro tid:293
-# Fix retrieve password scenarios @home +Diaspora tid:292
-# [Significant coding] for Diaspora @home +Diaspora tid:275
+(A) Thank Mom for the meatballs @phone
+(B) Schedule Goodwill pickup +GarageSale @phone
+Post signs around the neighborhood +GarageSale
+@GroceryStore Eskimo pies
+x 2011-03-02 2011-03-01 Review Tim's pull request +TodoTxtTouch @github
+Get things done @home
+Get some other things done @work
+Add task text to Chromodoro @home +Chromodoro tid:293
+Fix retrieve password scenarios @home +Diaspora tid:292
+[Significant coding] for Diaspora @home +Diaspora tid:275
