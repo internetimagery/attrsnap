d3b09a31a036b2b72d6f80cccbeb04c810d4c0c2
Jason Dixon <jdixon@wetafx.co.nz>
2017-11-08 17:26:42 +1300

    phew

--- table.py
+++ table.py
@@ -3,18 +3,17 @@ import maya.cmds as cmds
 import functools
 
 class Table(object):
-    def __init__(s, parent, cols, row):
+    def __init__(s, parent, cols, rows):
         """ Initiate a table.
         cols = ["name", "name"]
-        row = func() """
+        rows = func() """
         s.items = []
         s.cols = cols
-        s.row = row
+        s.rows = rows
         s.parent = parent
         s.dir = False
         s.order = 0
         s.sort(0)
-
 
     def sort(s, order, *_):
         """ Change sorting order """
@@ -28,14 +27,14 @@ class Table(object):
         old = cmds.layout(s.parent, q=True, ca=True)
         if old:
             cmds.deleteUI(old)
-        row = cmds.rowLayout(nc=len(s.cols))
+        row = cmds.rowsLayout(nc=len(s.cols))
         cols = [cmds.columnLayout(adj=True, p=row) for a in s.cols]
         for i, (name, col) in enumerate(zip(s.cols, cols)):
             dir_ = u"\u02C6" if s.dir else u"\u02c7"
             title = u"{} {}".format(name, dir_) if i == s.order else name
             cmds.button(l=title, p=col, c=functools.partial(s.sort, i))
         for row in sorted(s.items, key=(lambda x: x[s.order]), reverse=s.dir):
-            for gui, col in zip(s.row(row), cols):
+            for gui, col in zip(s.rows(row), cols):
                 cmds.control(gui, e=True, p=col)
 
 def main():
@@ -48,7 +47,7 @@ def main():
     cmds.window()
     p = cmds.columnLayout(adj=True)
     cmds.showWindow()
-    t = Table(p, ["num1", "num2"], lambda x: (cmds.text(l=x[0]), cmds.text(l=x[1])))
+    t = Table(p, ["num1", "num2"], lambda x: cmds.intField(v=x[0]), cmds.text(l=x[1]))
     t.items = rows
     t.build()
 
