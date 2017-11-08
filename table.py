01937932d9175b171f0990caeb646b3e95946a57
Jason Dixon <jdixon@wetafx.co.nz>
2017-11-08 16:45:43 +1300

    unicode

--- table.py
+++ table.py
@@ -3,14 +3,17 @@ import maya.cmds as cmds
 import functools
 
 class Table(object):
-    def __init__(s, parent, cols):
-        """ Initiate a table. cols = {"column", func} """
+    def __init__(s, parent, cols, row):
+        """ Initiate a table.
+        cols = ["name", "name"]
+        row = func() """
         s.items = []
         s.cols = cols
+        s.row = row
         s.parent = parent
-        s.dir = True
-        s.order = ""
-        s.sort(cols.keys()[0])
+        s.dir = False
+        s.order = 0
+        s.sort(0)
 
 
     def sort(s, order, *_):
@@ -27,27 +30,25 @@ class Table(object):
             cmds.deleteUI(old)
         row = cmds.rowLayout(nc=len(s.cols))
         cols = [cmds.columnLayout(adj=True, p=row) for a in s.cols]
-        for name, col in zip(s.cols, cols):
+        for i, (name, col) in enumerate(zip(s.cols, cols)):
             dir_ = u"\u02C6" if s.dir else u"\u02c7"
-            title = u"{} {}".format(name, dir_) if name == s.order else name
-            cmds.button(l=title, p=col, c=functools.partial(s.sort, name))
-        for row in sorted(s.items, key=s.cols[s.order], reverse=s.dir):
-            for name, col in zip(row, cols):
-                cmds.text(l=name, p=col)
+            title = u"{} {}".format(name, dir_) if i == s.order else name
+            cmds.button(l=title, p=col, c=functools.partial(s.sort, i))
+        for row in sorted(s.items, key=(lambda x: x[s.order]), reverse=s.dir):
+            for gui, col in zip(s.row(row), cols):
+                cmds.control(gui, e=True, p=col)
 
 def main():
-    cmds.window()
-    p = cmds.columnLayout(adj=True)
-    cmds.showWindow()
 
     rows = [
         (1, "one"),
         (2, "two"),
         (3, "three")]
 
-    t = Table(p, {
-        "num1": lambda x: x[0],
-        "num2": lambda x: x[1]})
+    cmds.window()
+    p = cmds.columnLayout(adj=True)
+    cmds.showWindow()
+    t = Table(p, ["num1", "num2"], lambda x: (cmds.text(l=x[0]), cmds.text(l=x[1])))
     t.items = rows
     t.build()
 
