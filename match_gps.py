b543c9575545de8f52124687b087607426b11c97
Jason Dixon <jdixon@wetafx.co.nz>
2017-11-13 18:30:04 +1300

    more tests

--- match_gps.py
+++ match_gps.py
@@ -4,12 +4,20 @@
 # https://github.com/gheja/trilateration.js/blob/master/trilateration.js
 from __future__ import division
 
+def div(a, b):
+    return b and a / b
+
+def sqrt(a):
+    if a < 0:
+        raise ValueError("value less than zero")
+    return a and ((a ** -0.5) * a)
+
 def sqr(a):
     return a * a
 
 def norm(a):
     mag = dot(a,a)
-    return (mag ** -0.5) * mag
+    return sqrt(mag)
 
 def dot(a, b):
     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
@@ -21,7 +29,7 @@ def vector_add(a, b):
     return a[0] + b[0], a[1] + b[1], a[2] + b[2]
 
 def vector_divide(a, b):
-    return a[0] / b, a[1] / b, a[2] / b
+    return div(a[0], b), div(a[1], b), div(a[2], b)
 
 def vector_multiply(a, b):
     return a[0] * b, a[1] * b, a[2] * b
@@ -50,8 +58,8 @@ def trilateration(P1, P2, P3, D1, D2, D3, return_middle=False):
 
     # Get location
     D1_sqr = sqr(D1)
-    x = (D1_sqr - sqr(D2) + sqr(d)) / (2 * d)
-    y = (D1_sqr - sqr(D3) + sqr(i) + sqr(j)) / (2 * j) - (i / j) * x
+    x = div((D1_sqr - sqr(D2) + sqr(d)), (2 * d))
+    y = div((D1_sqr - sqr(D3) + sqr(i) + sqr(j)), (2 * j)) - div(i, j) * x
 
     b = D1_sqr - sqr(x) - sqr(y)
 
@@ -59,9 +67,11 @@ def trilateration(P1, P2, P3, D1, D2, D3, return_middle=False):
     if abs(b) < 0.0000000001:
         b = 0
 
-    if b < 0:
+    try:
+        z = sqrt(b)
+    except ValueError:
         return []
-    z = (b ** -0.5) * b if b else 0.0
+
     a = vector_add(P1, vector_add(vector_multiply(ex, x), vector_multiply(ey, y)))
     ez_z = vector_multiply(ez, z)
     p4a = vector_add(a, ez_z)
@@ -79,7 +89,8 @@ def test():
     import time
 
     s = time.time()
-    for _ in range(100):
+    failed = []
+    for _ in range(100000):
 
         Position = lambda: [random.randrange(-10, 10) for _ in range(3)]
 
@@ -89,7 +100,16 @@ def test():
 
         prediction = trilateration(*args)
         if len(prediction) == 2:
-            prediction = min(prediction, key=lambda x: math.sqrt(sum((b-a)**2 for a,b in zip(x, emitter))))
+            prediction = [min(prediction, key=lambda x: math.sqrt(sum((b-a)**2 for a,b in zip(x, emitter))))]
+        pred = [round(b, 3) for a in prediction for b in a]
         # print "Actual position:", emitter
-        # print "Predicted position:", [round(a, 3) for a in prediction]
+        # print "Predicted position:", pred
+        try:
+            assert emitter == pred
+        except AssertionError:
+            failed.append((emitter, pred))
     print "Took", time.time() - s
+    print "Failed %s times" % len(failed)
+    for em, pred in failed:
+        print "Failed emitter:", em
+        print "Failed prediction:", pred
