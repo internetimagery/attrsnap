e5282cac027a3d7b0472f8dd337af5fda07e235f
Jason Dixon <jason.dixon.email@gmail.com>
2017-11-28 22:29:06 +1300

    working!

--- curve.py
+++ curve.py
@@ -27,19 +27,17 @@ def match(points):
     ERR = lambda A,B,C: sum((curve(A,B,C,x)-y)**2 for x,y,z in points)
     vals = [1,1,1]
 
-    rate = 0.01
+    rate = 0.00001
     prev_err = closest_err = ERR(*vals)
     closest_values = vals
 
-    for i in range(10):
+    for i in range(100000)
 
         # Check if we have overshot our target.
         # If so, reduce our sample rate because we are close.
         # Also reduce our momentum so we can turn faster.
         err = ERR(*vals)
         grad = gradient(vals, ERR)
-        print "ERROR", err
-        print "GRADIENT", grad
         # if err > prev_err:
         #     rate *= 0.5
         # prev_err = err
@@ -47,20 +45,19 @@ def match(points):
         # Check if we are closer than ever before.
         # Record it if so.
         if err < closest_err:
-            print "CLOSER", err, vals
             closest_err = err
             closest_values = vals
 
         # Check if we are stable enough to stop.
         # If rate is low enough we're not going to move anywhere anyway...
-        if rate < 0.00001:
-            print("Rate below tolerance. Done.")
-            break
+        # if rate < 0.00001:
+        #     print("Rate below tolerance. Done.")
+        #     break
 
         # Check if we are sitting on a flat plateau.
         try:
             if sum((g-p)**2 for g,p in zip(grad, prev_grad)) < 0.0001:
-                print("Gradient flat. Done.")
+                print("Gradient flat. Done. After %s tries" % i)
                 break
         except NameError:
             pass
@@ -68,7 +65,8 @@ def match(points):
 
         vals = [-rate * g + v for v, g in zip(vals, grad)]
 
-    return vals
+    print "closest", closest_err
+    return closest_values
 
 
 
@@ -93,7 +91,7 @@ def test():
     A,B,C = match(points)
 
     print "Result", A,B,C
-    print points[3][1], curve(delta[0],delta[1],delta[2],points[3][0])
+    print points[3][1], curve(A,B,C,points[3][0])
 
     # crv = cmds.curve(p=(0,0,0))
     for i in range(-10, 11):
