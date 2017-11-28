495a7f5bb59c603aaa3298d2689f40df8e702cdc
Jason Dixon <jason.dixon.email@gmail.com>
2017-11-28 22:18:23 +1300

    hmm

--- curve.py
+++ curve.py
@@ -27,26 +27,27 @@ def match(points):
     ERR = lambda A,B,C: sum((curve(A,B,C,x)-y)**2 for x,y,z in points)
     vals = [1,1,1]
 
-    rate = 0.8
-    friction = 0.8
-    velocity = [0]*len(vals)
+    rate = 0.01
     prev_err = closest_err = ERR(*vals)
     closest_values = vals
 
-    for i in range(1):
+    for i in range(10):
 
         # Check if we have overshot our target.
         # If so, reduce our sample rate because we are close.
         # Also reduce our momentum so we can turn faster.
         err = ERR(*vals)
-        if err > prev_err:
-            rate *= 0.5
-            velocity = [a*0.5 for a in velocity]
-        prev_err = err
+        grad = gradient(vals, ERR)
+        print "ERROR", err
+        print "GRADIENT", grad
+        # if err > prev_err:
+        #     rate *= 0.5
+        # prev_err = err
 
         # Check if we are closer than ever before.
         # Record it if so.
         if err < closest_err:
+            print "CLOSER", err, vals
             closest_err = err
             closest_values = vals
 
@@ -57,7 +58,6 @@ def match(points):
             break
 
         # Check if we are sitting on a flat plateau.
-        grad = gradient(vals, ERR)
         try:
             if sum((g-p)**2 for g,p in zip(grad, prev_grad)) < 0.0001:
                 print("Gradient flat. Done.")
@@ -66,11 +66,9 @@ def match(points):
             pass
         prev_grad = grad
 
-        # Update our momentum
-        prev_velocity = velocity
-        velocity = [v * friction - g * rate for v, g in zip(velocity, grad)]
-        vals = [p * -friction + v * (1+friction) for p, v in zip(prev_velocity, velocity)]
+        vals = [-rate * g + v for v, g in zip(vals, grad)]
 
+    return vals
 
 
 
@@ -92,16 +90,15 @@ def test():
         cmds.spaceLocator(p=pnt)
         points.append(pnt)
 
-    delta = match(points)
+    A,B,C = match(points)
 
+    print "Result", A,B,C
+    print points[3][1], curve(delta[0],delta[1],delta[2],points[3][0])
 
-
-
-    # # crv = cmds.curve(p=(0,0,0))
-    # for i in range(-10, 11):
-    #     a,b,c = 3,5,0.8
-    #     pos = (i, curve(a,b,c,i), 0)
-    #     try:
-    #         cmds.curve(crv, a=True, p=pos)
-    #     except NameError:
-    #         crv = cmds.curve(p=pos)
+    # crv = cmds.curve(p=(0,0,0))
+    for i in range(-10, 11):
+        pos = (i, curve(A,B,C,i), 0)
+        try:
+            cmds.curve(crv, a=True, p=pos)
+        except NameError:
+            crv = cmds.curve(p=pos)
