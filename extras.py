be2ef368311c05f0b07160f184643c535720c84e
Jason Dixon <jdixon@wetafx.co.nz>
2018-03-21 10:16:45 +1300

    extras item

--- extras.py
+++ extras.py
@@ -1,18 +1,26 @@
 # Extra functionality for niche situations
 
 
-# WIP
-import utility
-import gui
+def FROM_TO_Namespace():
+    """ Load match file. Replace "FROM:" and "TO:"
+        with selected objects namespaces. Open mini window.
+        Maya specific.
+    """
+    import gui_maya as gui
+    import utility_maya as utility
 
-def quick_B_to_A():
-    """ Quick selection matching """
     selection = utility.get_selection(2)
     templates = utility.load_prompt()
-    # TODO: Get namespace of selected
-    # TODO: rename "TO" and "FROM" to namespaces
+
+    ns_select = []
+    for select in selection:
+        parts = select.rsplit(":", 1)
+        ns_select.append(parts[0]+":" if len(parts) == 2 else "")
+
     for template in templates:
-        pass
+        template.markers = [a.replace("FROM:", ns_select[0]).replace("TO:", ns_select[1]) for a in template.markers]
+        template.attributes = [{a: b.replace("FROM:", ns_select[0]).replace("TO:", ns_select[1]) if a == "obj" else b for a,b in a.items()} for a in template.attributes]
+
     fix = Fixer(templates, gui.MiniWindow)
     if not fix.missing:
         gui.MiniWindow(templates)
